a3418eaae82d6733fe6aff19c2971d9072a4426d
Paweł Wydrych <pawel.wydrych2002@gmail.com>
2025-03-16 22:54:21 +0100

    Add State property to Order class

--- PromiseGroupRecruitmentTask/Order.cs
+++ PromiseGroupRecruitmentTask/Order.cs
@@ -1,15 +1,19 @@
+using PromiseGroupRecruitmentTask.Enums;
+
 namespace PromiseGroupRecruitmentTask;
 
 public class Order
 {
+    public OrderState State { get; set; }
     public double Amount { get; set; }
     public string Name { get; set; }
     public ClientType ClientType { get; set; }
     public string Address { get; set; }
     public PaymentType PaymentType { get; set; }
 
-    public Order(double amount, string name, ClientType clientType, string address, PaymentType paymentType)
+    public Order(OrderState state, double amount, string name, ClientType clientType, string address, PaymentType paymentType)
     {
+        State = state;
         Amount = amount;
         Name = name;
         ClientType = clientType;
