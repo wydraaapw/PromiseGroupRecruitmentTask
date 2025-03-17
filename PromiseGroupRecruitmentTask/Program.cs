aef9e11bdc05a7fd17621986efb49d71e9a5247b
Paweł Wydrych <pawel.wydrych2002@gmail.com>
2025-03-17 23:45:46 +0100

    Update Main method of Program

--- PromiseGroupRecruitmentTask/Program.cs
+++ PromiseGroupRecruitmentTask/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace PromiseGroupRecruitmentTask;
+﻿using PromiseGroupRecruitmentTask.Enums;
+using PromiseGroupRecruitmentTask.Repositories;
+
+namespace PromiseGroupRecruitmentTask;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        IOrderRepository orderRepository = new OrderRepository();
+        OrderService orderService = new OrderService(orderRepository);
+        UserInterface userInterface = new UserInterface(orderService);
         
+        userInterface.Run();
     }
 }
